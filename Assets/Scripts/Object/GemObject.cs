1126bba9b726fd737e5b946baacbc22102a5fa11
digital8150 <wptpgud@gmail.com>
2025-12-31 13:55:29 +0900

    feature: 보석 오브젝트 스크립트

---
+++ Assets/Scripts/Object/GemObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GemObject : SpecialObject
+{
+    [Header("Components")]
+    [SerializeField] private StageManager stageManager;
+
+    protected override void ApplyEffect(GameObject player)
+    {
+        stageManager.CollectGem();
+    }
+}
