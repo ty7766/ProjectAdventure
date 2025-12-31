ee6068141070071ed5e72b6c9fb04da1d2b1c102
digital8150 <wptpgud@gmail.com>
2025-12-31 12:46:10 +0900

    feature: 특수 오브젝트 추상 클래스

---
+++ Assets/Scripts/Object/SpecialObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class SpecialObject : MonoBehaviour
+{
+    //--- Components ---//
+    protected Collider col;
+    protected Renderer rend;
+
+    //--- Fields ---//
+    [Header("속성")]
+    [SerializeField] private bool isRespawnable = false;
+    [SerializeField] private float respawnTime = 5f;
+
+
+    //--- Unity Methods ---//
+    protected virtual void Start()
+    {
+        if(!TryGetComponent<Collider>(out col))
+        {
+            Debug.LogWarning("Collider Component not found on SpecialObject");
+        }
+        if(!TryGetComponent<Renderer>(out rend))
+        {
+            Debug.LogWarning("Renderer Component not found on SpecialObject");
+        }
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        // 태그가 Player인 녀석만 반응하도록 필터링
+        if (other.CompareTag("Player"))
+        {
+            // 1. 자식 클래스에서 정의한 실제 효과 발동!
+            ApplyEffect(other.gameObject);
+            if(isRespawnable)
+            {
+                // 2. 오브젝트 비활성화
+                col.enabled = false;
+                rend.enabled = false;
+                // 3. 일정 시간 후 재생성
+                StartCoroutine(RespawnAfterSeconds(respawnTime));
+            }
+            else
+            {
+                // 오브젝트 파괴
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    protected IEnumerator RespawnAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        col.enabled = true;
+        rend.enabled = true;
+    }
+
+    protected abstract void ApplyEffect(GameObject player);
+}
