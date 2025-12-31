c8920302c1f9477350fe0cc87899c63056083485
digital8150 <wptpgud@gmail.com>
2025-12-31 13:54:59 +0900

    feature: 기본 스테이지 메니저 (습득해야 하는 보석 개수,  기본 클리어 처리)

---
+++ Assets/Scripts/Stage/StageManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private PlayerController playerController;
+
+    [Header("Stage Settings")]
+    [SerializeField] private int requiredGemsToClear = 1;
+    private int collectedGems = 0;
+
+    //--- Public Methods ---//
+    public void CollectGem()
+    {
+        collectedGems++;
+        if (collectedGems >= requiredGemsToClear)
+        {
+            ClearStage();
+        }
+    }
+
+    //--- Private Helpers ---//
+    private void ClearStage()
+    {
+        // Implement stage clear logic here (e.g., show UI, stop player movement)
+        Debug.Log("Stage Cleared!");
+        playerController.DisableMovement();
+    }
+}
