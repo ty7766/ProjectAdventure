cf480ce5c90e1a658d4b9032c6c55aafb71b7ee1
hyojoon Lim <hyojoon42@naver.com>
2026-01-08 00:18:23 +0900

    feature : 다 쓴 VFX를 풀에 반납하는 스크립트 추가

---
+++ Assets/02. Scripts/VFX/VFXReturnToPool.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+public class VFXReturnToPool : MonoBehaviour
+{
+    private VFXType _myType;
+    private ParticleSystem _ps;
+
+    private void Awake()
+    {
+        _ps = GetComponent<ParticleSystem>();
+    }
+
+    /// <summary>
+    /// 생성 시 Manager가 Type을 알려주는 메소드
+    /// </summary>
+    /// <param name="type">VFX타입</param>
+    public void Setup(VFXType type)
+    {
+        _myType = type;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckIfAlive());
+    }
+
+    private IEnumerator CheckIfAlive()
+    {
+        //파티클이 재생중이면 대기
+        if (_ps != null)
+        {
+            yield return new WaitWhile(() => _ps.IsAlive(true));
+        }
+        else
+        {
+            yield return new WaitForSeconds(2.0f);
+        }
+
+        //파티클 끝나면 반납
+        VFXManager.Instance.ReturnToPool(_myType, this.gameObject);    }
+}
