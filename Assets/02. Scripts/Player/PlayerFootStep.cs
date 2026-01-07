23e400cfb1c7a6dba76e77182709ec8bc5311ba2
hyojoon Lim <hyojoon42@naver.com>
2026-01-08 00:46:33 +0900

    feature : 플레이어의 다리 부분과 이펙트를 연결하는 스크립트 추가

---
+++ Assets/02. Scripts/Player/PlayerFootStep.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerFootstep : MonoBehaviour
+{
+    [Header("발 뼈대 연결 (Hierarchy에서 드래그)")]
+    [SerializeField] 
+    private Transform _leftFoot;  //Toe.Ctrl.L
+    [SerializeField] 
+    private Transform _rightFoot; //Toe.Ctrl.R
+
+    [Header("설정")]
+    [SerializeField] 
+    private LayerMask _groundLayer;
+    [SerializeField] 
+    private float _rayDistance = 0.3f;
+
+
+    /// <summary>
+    /// 애니메이션 이벤트에서 호출할 함수 (0: 왼쪽, 1: 오른쪽)
+    /// </summary>
+    /// <param name="isLeft">왼쪽발 오른쪽발 구분하기 위한 변수</param>
+    public void OnFootstep(int isLeft)
+    {
+        Transform foot = (isLeft == 0) ? _leftFoot : _rightFoot;
+        if (foot == null)
+        {
+            return;
+        }
+
+        if (Physics.Raycast(foot.position + Vector3.up * 0.1f, Vector3.down, out RaycastHit hit, _rayDistance, _groundLayer))
+        {
+            if (System.Enum.TryParse(hit.collider.tag, out VFXType groundType))
+            {
+                //눈 이펙트 발생 실행
+                VFXManager.Instance.PlayVFX(groundType, hit.point + Vector3.up * 0.02f);
+            }
+        }
+    }
+}
