915ea8585d3b422fab0167534154635d9ed2ad14
digital8150 <wptpgud@gmail.com>
2026-01-16 16:10:47 +0900

    feature: 플레이어 체력 HUD 기능

---
+++ Assets/02. Scripts/UI/HUD/HUDPresenter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class HUDPresenter : MonoBehaviour
+{
+    //--- Settings ---//
+    [Header("References")]
+    [SerializeField] private PlayerProperties _playerModel;
+    [SerializeField] private HUDView _healthView;
+
+    //--- Unity Methods ---//
+    private void Start()
+    {
+        SubscribeEventHandlers();
+    }
+
+    private void SubscribeEventHandlers()
+    {
+        UnsubscribeEventHandlers();
+    }
+
+    //--- Private Methods ---//
+    private void UnsubscribeEventHandlers()
+    {
+        if (_playerModel != null)
+        {
+            _healthView.UpdateHealthUI(_playerModel.Health);
+            _playerModel.OnHealthChanged += HandleHealthChanged;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_playerModel != null)
+        {
+            _playerModel.OnHealthChanged -= HandleHealthChanged;
+        }
+    }
+
+    private void HandleHealthChanged(int newHealth)
+    {
+        _healthView.UpdateHealthUI(newHealth);
+    }
+}
---
+++ Assets/02. Scripts/UI/HUD/HUDView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HUDView : MonoBehaviour
+{
+    //--- Settings ---//
+    [Header("UI Components")]
+    [SerializeField] private List<Image> _heartImages; // 하트 아이콘 리스트
+    [SerializeField] private Sprite _fullHeart; // 꽉 찬 하트 이미지
+    [SerializeField] private Sprite _emptyHeart; // 빈 하트 이미지
+
+    //--- Public Methods ---//
+    /// <summary>
+    /// UI의 체력 표시를 업데이트 합니다.
+    /// </summary>
+    /// <param name="currentHealth"></param>
+    public void UpdateHealthUI(int currentHealth)
+    {
+        for (int i = 0; i < _heartImages.Count; i++)
+        {
+            if (i < currentHealth)
+            {
+                _heartImages[i].sprite = _fullHeart;
+            }
+            else
+            {
+                _heartImages[i].sprite = _emptyHeart;
+            }
+        }
+    }
+}
