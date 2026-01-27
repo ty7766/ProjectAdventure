cfa6a5a3f8d8d9ba6c1f3e2ab291421504060a34
digital8150 <wptpgud@gmail.com>
2026-01-28 06:49:43 +0900

    feature: 타이틀 화면 MVP 패턴, 게임 종료 버튼 구현

---
+++ Assets/02. Scripts/UI/Title/TitlePresenter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TitlePresenter
+{
+    //--- Fields ---//
+    private TitleView _titleView;
+
+    public TitlePresenter(TitleView titleView)
+    {
+        _titleView = titleView;
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        // Handle play button click logic here
+    }
+
+    public void OnOptionsButtonClicked()
+    {
+        // Handle options button click logic here
+    }
+
+    public void OnExitButtonClicked()
+    {
+        // Handle exit button click logic here
+        ExitGame();
+    }
+
+    void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+
+}
