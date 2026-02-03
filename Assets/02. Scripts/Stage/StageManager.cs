86d72e577e2d3e30326e0ba1a98545ee977c0cd0
digital8150 <wptpgud@gmail.com>
2026-02-03 23:13:53 +0900

    refactor: StageObject.cs 메서드 추출 기법 적용, 일부 메서드 이름 변경

--- Assets/02. Scripts/Stage/StageManager.cs
+++ Assets/02. Scripts/Stage/StageManager.cs
@@ -55,22 +55,14 @@ public class StageObject
     //--- Unity Methods ---//
     private void Awake()
     {
-        if (Instance == null)
+        bool isInstanceNull = TryInitializeSingleton();
+        if (!isInstanceNull)
         {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
             return;
         }
 
-        if (_playerController != null) {
-            _playerController.OnPlayerDamageTaken += HandlePlayerDamageTakenEvent;
-            _playerController.OnPlayerFallenDown += HandlePlayerFallenDownEvent;
-        }
-
-        ApplyAlreadyClearedStageObjects();
+        SubscribeEvents();
+        LoadStageObjectStatusFromSave();
     }
 
     private void OnDestroy() 
@@ -149,6 +141,29 @@ public class StageObject
 
 
     //--- Private Helpers ---//
+    private bool TryInitializeSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            return true;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return false;
+        }
+    }
+
+    private void SubscribeEvents()
+    {
+        if (_playerController != null)
+        {
+            _playerController.OnPlayerDamageTaken += HandlePlayerDamageTakenEvent;
+            _playerController.OnPlayerFallenDown += HandlePlayerFallenDownEvent;
+        }
+    }
+
     private IEnumerator ChangeTimeScale(float targetScale, float duration)
     {
         float startScale = Time.timeScale;
@@ -178,9 +193,17 @@ public class StageObject
 
     private void ClearStage()
     {
-        CustomDebug.Log("Stage Cleared!");
         DisablePlayerControl();
-        foreach(var obj in _stageObjects)
+
+        CheckStageObject();
+        OnStageCleared?.Invoke();
+
+        SaveStageClearData();
+    }
+
+    private void CheckStageObject()
+    {
+        foreach (var obj in _stageObjects)
         {
             if (obj.isCleared)
             {
@@ -189,37 +212,34 @@ public class StageObject
             switch (obj.stageObjectType)
             {
                 case StageObjectType.NoDamageClear:
-                    if(!_isPlayerDamageTaken)
+                    if (!_isPlayerDamageTaken)
                     {
                         obj.isCleared = true;
                     }
                     break;
 
                 case StageObjectType.NoFallClear:
-                    if(!_isPlayerFallenDown)
+                    if (!_isPlayerFallenDown)
                     {
                         obj.isCleared = true;
                     }
                     break;
 
                 case StageObjectType.TimeLimitClear:
-                    if(_stageTimer <= obj.value)
+                    if (_stageTimer <= obj.value)
                     {
                         obj.isCleared = true;
                     }
                     break;
 
                 case StageObjectType.RemainHealthClear:
-                    if(_playerController != null && _playerController.Health >= obj.value)
+                    if (_playerController != null && _playerController.Health >= obj.value)
                     {
                         obj.isCleared = true;
                     }
                     break;
             }
         }
-
-        OnStageCleared?.Invoke();
-        SaveStageClearData();
     }
 
     private void DisablePlayerControl()
@@ -264,7 +284,7 @@ public class StageObject
         _isPlayerFallenDown = true;
     }
 
-    private void ApplyAlreadyClearedStageObjects()
+    private void LoadStageObjectStatusFromSave()
     {
         foreach(var obj in _stageObjects)
         {
