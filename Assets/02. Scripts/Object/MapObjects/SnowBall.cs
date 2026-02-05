ae3961be3c1aa16c6cae9d23540b83cdc054c0e2
hyojoon Lim <hyojoon42@naver.com>
2026-02-05 16:04:36 +0900

    feature : SnowBall 기믹 코드 추가 with Object Pooling

---
+++ Assets/02. Scripts/Object/MapObjects/SnowBall.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
+public class SnowBall : MonoBehaviour
+{
+    [Header("충돌 설정")]
+    [SerializeField]
+    private int _damageAmount = 1;
+
+    [Header("VFX 설정")]
+    [SerializeField]
+    private VFXType _destroyVFXType = VFXType.SnowBallHit;
+
+    [Header("오브젝트 풀링 설정")]
+    [SerializeField]
+    private PoolObjectType _objectType = PoolObjectType.SnowBall;
+    [SerializeField]
+    private float _lifeTime = 10f;
+
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        if (_rb != null)
+        {
+            _rb.linearVelocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+            _rb.Sleep();
+            _rb.WakeUp();
+        }
+        StartCoroutine(ActivateSnowBallRoutine());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HandlePlayerHit(collision);
+        }
+    }
+
+    private void HandlePlayerHit(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent<PlayerController>(out var playerController))
+        {
+            playerController.TakeDamage(_damageAmount);
+        }
+
+        if(VFXManager.Instance != null)
+        {
+            ContactPoint contact = collision.GetContact(0);
+            VFXManager.Instance.PlayVFX(_destroyVFXType, contact.point, Quaternion.LookRotation(contact.normal));
+        }
+
+        ReturnToPool();
+    }
+
+    private IEnumerator ActivateSnowBallRoutine()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+        ReturnToPool();
+    }
+
+    private void ReturnToPool()
+    {
+        StopAllCoroutines();
+
+        if(ObjectPoolManager.Instance != null)
+        {
+            ObjectPoolManager.Instance.ReturnObject(_objectType, this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/02. Scripts/Object/MapObjects/SnowBallSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class SnowBallSpawner : MonoBehaviour
+{
+    [Header("생성 설정")]
+    [SerializeField]
+    private PoolObjectType _snowBallType = PoolObjectType.SnowBall;
+    [SerializeField] 
+    private Transform _spawnPoint;
+
+    [Header("물리 설정")]
+    [SerializeField] 
+    private Vector3 _initialForce = new Vector3(0, -2f, 10f);
+
+    [Header("트리거 설정")]
+    [SerializeField]
+    private bool _isOneTimeUse = true;
+
+    private void Awake()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpawnSnowBall();
+        }
+    }
+
+    private void SpawnSnowBall()
+    {
+        if (ObjectPoolManager.Instance == null || _spawnPoint == null)
+        {
+            return;
+        }
+
+        GameObject snowBall = ObjectPoolManager.Instance.SpawnObject(_snowBallType, _spawnPoint.position, _spawnPoint.rotation);
+
+        if (snowBall != null && snowBall.TryGetComponent<Rigidbody>(out var rb))
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.AddForce(_initialForce, ForceMode.Impulse);
+        }
+
+        if (_isOneTimeUse)
+        {
+            GetComponent<Collider>().enabled = false;
+            this.enabled = false;
+        }
+    }
+}
