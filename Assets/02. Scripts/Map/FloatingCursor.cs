7b15e08010d73030826c89bdd0749beca1911c78
hyojoon Lim <hyojoon42@naver.com>
2026-02-05 23:18:27 +0900

    feature : 커서 애니메이션 추가

---
+++ Assets/02. Scripts/Map/FloatingCursor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FloatingCursor : MonoBehaviour
+{
+    [Header("애니메이션 설정")]
+    [SerializeField]
+    private float _speed = 2f;
+    [SerializeField]
+    private float _height = 0.5f;
+
+    private Vector3 _basePosition;
+    private bool _isInitialized = false;
+
+    private void Update()
+    {
+        if(!_isInitialized)
+        {
+            return;
+        }
+
+        float newY = _basePosition.y + Mathf.Sin(Time.time * _speed) * _height;
+
+        //y축만 수직 운동
+        transform.position = new Vector3 (_basePosition.x, newY, _basePosition.z);
+    }
+
+    public void SetBasePositionCursor(Vector3 newPosition)
+    {
+        _basePosition = newPosition;
+        _isInitialized = true;
+        transform.position = _basePosition;
+    }
+}
