d73a4490a66a713e649bc410d8cbce14927d832e
hyojoon Lim <hyojoon42@naver.com>
2026-02-12 22:44:48 +0900

    fix : 플레이어 레이어를 받아서 불필요한 다른 오브젝트들의 레이어를 검출하지 않도록 변경

--- Assets/02. Scripts/Map/MapPlayerChecker.cs
+++ Assets/02. Scripts/Map/MapPlayerChecker.cs
@@ -2,8 +2,16 @@
 
 public class MapPlayerChecker : MonoBehaviour
 {
-    const float DETECTIONY = 200;       //체크할 높이
+    [Header("플레이어 레이어")]
+    [SerializeField]
+    private LayerMask _playerLayer;
+
+    [Header("검출할 Y 높이")]
+    [SerializeField]
+    private float _detectionHeight = 200;       //체크할 높이
+
     private Collider[] _hitBuffer = new Collider[100];
+
     /// <summary>
     /// 플레이어가 맵 위에 올라가 있는지 체크하는 메소드
     /// </summary>
@@ -16,13 +24,14 @@ public class MapPlayerChecker : MonoBehaviour
         {
             return false;
         }
-        Vector3 checkSize = tileSize + Vector3.up * DETECTIONY;
+        Vector3 checkSize = tileSize + Vector3.up * _detectionHeight;
 
         int hitCount = Physics.OverlapBoxNonAlloc(
             group.SpawnPoint.position,
             checkSize * 0.5f,
             _hitBuffer,
-            group.SpawnPoint.rotation
+            group.SpawnPoint.rotation,
+            _playerLayer
         );
 
         for (int i = 0; i < hitCount; i++)
