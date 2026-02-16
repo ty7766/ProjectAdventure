497cee3d16e594904add08a0007ad891fbf10370
hyojoon Lim <hyojoon42@naver.com>
2026-02-16 19:38:16 +0900

    feature : 젬의 회전 애니메이션 추가

--- Assets/02. Scripts/Object/MapObjects/Gem/GemAnimation.cs
+++ Assets/02. Scripts/Object/MapObjects/Gem/GemAnimation.cs
@@ -1,16 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GemAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("회전 속도/각도 설정")]
+    [SerializeField]
+    private Vector3 _rotationAxis = Vector3.up;
+    [SerializeField]
+    private float _rotationSpeed = 50.0f;
+
+    [Tooltip("떠다니는 높이 범위")]
+    [SerializeField]
+    private float _floatAmplitude = 0.25f;
+
+    [Tooltip("떠다니는 속도")]
+    [SerializeField]
+    private float _floatFrequency = 1.0f;
+
+    private Vector3 _initialLocalPosition;
+
+    private void Start()
     {
-        
+        _initialLocalPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ApplyRotationForGem();
+        ApplyFloatingForGem();
+    }
+
+    private void ApplyRotationForGem()
+    {
+        transform.Rotate(_rotationAxis * _rotationSpeed * Time.deltaTime);
+    }
+
+    private void ApplyFloatingForGem()
+    {
+        float newY = _initialLocalPosition.y + (Mathf.Sin(Time.time * _floatFrequency) * _floatAmplitude);
+
+        Vector3 newPosition = new Vector3(_initialLocalPosition.x, newY, _initialLocalPosition.z);
+        transform.localPosition = newPosition;
     }
 }
