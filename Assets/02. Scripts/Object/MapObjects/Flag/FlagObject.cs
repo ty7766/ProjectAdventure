335e12b3a5d998f458edc44100e6560816d7cc74
digital8150 <wptpgud@gmail.com>
2026-02-20 02:51:01 +0900

    feature: 깃발 오브젝트

---
+++ Assets/02. Scripts/Object/MapObjects/Flag/FlagObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class FlagObject : SpecialObject
+{
+    [Header("Components")]
+    [SerializeField] private StageManager _stageManager;
+
+    private void Start()
+    {
+        FindStageManagerWhenIsNull();
+    }
+
+    protected override void ApplyEffect(GameObject player)
+    {
+        StageClear();
+    }
+
+    //--- Private Methods ---//
+    private void StageClear()
+    {
+        if (_stageManager == null)
+        {
+            FindStageManagerWhenIsNull();
+        }
+
+        if (_stageManager != null)
+        {
+            _stageManager.StageClear();
+        }
+        else
+        {
+            CustomDebug.LogWarning("StageManager reference is missing in GemObject.");
+        }
+    }
+
+    //인스펙터 할당이 불가능 할 경우 (맵 매니저에 의한 동적 생성 등) 씬에서 찾아 할당
+    private void FindStageManagerWhenIsNull()
+    {
+        if (_stageManager != null)
+        {
+            return;
+        }
+
+        _stageManager = FindAnyObjectByType<StageManager>();
+        if (_stageManager == null)
+        {
+            CustomDebug.LogError("StageManager not found in the scene for GemObject.", this);
+        }
+    }
+}
