13d8d1c68b09f933ef4082eede184f3e4dcb19b5
hyojoon Lim <hyojoon42@naver.com>
2026-02-22 03:13:00 +0900

    fix : 스테이지 마다 플레이어 추적 각도가 상이한 점 수정

--- Assets/02. Scripts/Camera/CameraFollow.cs
+++ Assets/02. Scripts/Camera/CameraFollow.cs
@@ -3,32 +3,39 @@
 public class CameraFollow : MonoBehaviour
 {
     [Header("따라갈 대상")]
-    [SerializeField] 
+    [SerializeField]
     private Transform _target;
 
     [Header("따라가는 속도")]
     [SerializeField]
     private float _smoothSpeed = 0.1f;
 
+    [Header("카메라 고정 오프셋")]
+    [SerializeField]
     private Vector3 _playerOffset;
 
-    void Start()
+    private void LateUpdate()
+    {
+        if (_target == null)
+        {
+            return;
+        }
+
+        Vector3 desiredPosition = _target.position + _playerOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+
+    [ContextMenu("현재 씬의 각도를 오프셋으로 저장")]
+    private void SaveCurrentOffset()
     {
         if (_target != null)
         {
             _playerOffset = transform.position - _target.position;
         }
-    }
-
-    void LateUpdate()
-    {
-        if (_target == null) return;
-
-        Vector3 desiredPosition = _target.position + _playerOffset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
-        transform.position = smoothedPosition;
-
-        //카메라는 항상 플레이어를 주시하고 싶다면 아래 주석 해제
-        // transform.LookAt(_target); 
+        else
+        {
+            CustomDebug.LogWarning("Target이 지정되지 않아 오프셋을 계산할 수 없습니다");
+        }
     }
 }
