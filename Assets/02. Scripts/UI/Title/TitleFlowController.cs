4ad6ea4ea00feacc14f054fa60f6eda4e1c43eab
digital8150 <wptpgud@gmail.com>
2026-02-25 22:45:35 +0900

    fix: Block Raycast 만 제어

--- Assets/02. Scripts/UI/Title/TitleFlowController.cs
+++ Assets/02. Scripts/UI/Title/TitleFlowController.cs
@@ -41,9 +41,9 @@ public class TitleFlowController : MonoBehaviour
     private void InitializeTitleView()
     {
         _stageSelectCanvas.blocksRaycasts = false;
-        _stageSelectCanvas.interactable = false;
+        _stageSelectCanvas.interactable = true;
         _titleCanvas.blocksRaycasts = false;
-        _titleCanvas.interactable = false;
+        _titleCanvas.interactable = true;
         _titleCanvas.alpha = 0.0f;
         _stageSelectCanvas.alpha = 0.0f;
         StartCoroutine(FadeIn(_titleCanvas, _introDuration));
@@ -52,7 +52,6 @@ public class TitleFlowController : MonoBehaviour
     private IEnumerator SequentialFade(CanvasGroup fadeOut, CanvasGroup fadeIn)
     {
         // 터치 컷
-        fadeOut.interactable = false;
         fadeOut.blocksRaycasts = false;
 
         // 전체 시간을 반으로 나눠서 페이드아웃, 페이드인에 각각 사용!
@@ -80,7 +79,6 @@ public class TitleFlowController : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(0.5f); // 잠깐 멈춤
         // 다 켜진 캔버스만 터치 온
-        fadeIn.interactable = true;
         fadeIn.blocksRaycasts = true;
     }
 
