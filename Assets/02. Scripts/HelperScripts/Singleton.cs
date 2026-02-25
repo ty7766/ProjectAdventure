655bfa55cf575e15a15d499b1960cb89947e8d7f
hyojoon Lim <hyojoon42@naver.com>
2026-02-25 19:05:38 +0900

    refactor : Lazy Initialization을 통해 널 예외 처리를 부모 클래스에서 실행

--- Assets/02. Scripts/HelperScripts/Singleton.cs
+++ Assets/02. Scripts/HelperScripts/Singleton.cs
@@ -7,13 +7,22 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         get
         {
+            //Lazy Initialization
+            if (_instance == null)
+            {
+                _instance = FindAnyObjectByType<T>();
+                if (_instance == null)
+                {
+                    CustomDebug.LogError($"씬에 {typeof(T).Name} 이 없습니다. 하이어라키 창에 올려주세요.");
+                }
+            }
             return _instance;
         }
     }
 
     protected virtual void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
             return;
