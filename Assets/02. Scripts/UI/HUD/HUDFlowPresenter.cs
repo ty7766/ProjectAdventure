52146d0668c9e278c16f7c64daa3ef59322bd5cc
hyojoon Lim <hyojoon42@naver.com>
2026-02-27 03:23:26 +0900

    feature : Countdown SFX 추가

--- Assets/02. Scripts/UI/HUD/HUDFlowPresenter.cs
+++ Assets/02. Scripts/UI/HUD/HUDFlowPresenter.cs
@@ -82,12 +82,16 @@ public class HUDFlowPresenter : MonoBehaviour
 
         for (int i = 3; i >= 1; i--)
         {
+            SoundManager.Instance.PlaySFX(SoundType.SFX_GameStartCountdown);
+
             _hudView.ApplyStageCountDownAnimation(128f, 0.5f);
             _hudView.UpdateStageCountDownContent(i.ToString());
             yield return new WaitForSecondsRealtime(0.5f);
             _hudView.ApplyStageCountDownAnimation(100f, 0.5f);
             yield return new WaitForSecondsRealtime(0.5f);
         }
+
+        SoundManager.Instance.PlaySFX(SoundType.SFX_GameStart);
 
         _hudView.UpdateStageCountDownContent(_stageGoString);
         _hudView.ApplyStageCountDownAnimation(120f, 0.2f);
