b65f2081bd01936498176ee1b611cb07321a6cb3
hyojoon Lim <hyojoon42@naver.com>
2026-02-27 03:22:57 +0900

    feature : SoundType 추가

--- Assets/02. Scripts/SFX/SoundType.cs
+++ Assets/02. Scripts/SFX/SoundType.cs
@@ -2,6 +2,26 @@
 public enum SoundType
 {
     None = 0,
-    BackgroundMusic,
-    ClearSound,
+    
+    //BackGroundMusic
+    BGM_TitleSceneMusic = 100,
+    BGM_BackGroundMusic = 101,
+
+    //UI SFX
+    SFX_ButtonClick = 200,
+    SFX_ClearUI = 201,
+    SFX_GameOverUI = 202,
+    SFX_GameStartCountdown = 203,
+    SFX_GameStart = 204,
+
+    //InGame SFX
+    SFX_MapSwitch = 300,
+    SFX_MapChange = 301,
+    SFX_GemCollect = 302,
+    SFX_FlagCollect = 303,
+
+    //Player SFX
+    SFX_PlayerDamaged = 401,
+    SFX_PlayerDead = 402,
+
 }
