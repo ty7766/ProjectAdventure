0daa37cfac3063dac487329db31dcbb65bee46c3
digital8150 <wptpgud@gmail.com>
2026-03-08 19:13:09 +0900

    feature: 옵션 메뉴 그래픽 설정 UI

---
+++ Assets/02. Scripts/UI/Options/UI_OptionPresenter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class UI_OptionPresenter
+{
+    private UI_OptionView _view;
+    private string _currentTab;
+    private List<UI_OptionTabButtonView> _tabButtons;
+
+    private float _unselectedTabOpacity;
+
+    public UI_OptionPresenter(UI_OptionView view, List<UI_OptionTabButtonView> tabButtons, float unselectedTabOpacity)
+    {
+        _view = view;
+        _tabButtons = tabButtons;
+        _unselectedTabOpacity = unselectedTabOpacity;
+        Initialize();
+    }
+
+    public void Start()
+    {
+        foreach (var tabButton in _tabButtons)
+        {
+            string tabName = tabButton.TabName;
+            tabButton.AddButtonListener(() => OnTabSelected(tabName));
+        }
+        _currentTab = _tabButtons[0].TabName; // Default to the first tab
+        UpdateTabButtonVisuals();
+    }
+
+    private void Initialize()
+    {
+
+    }
+
+    private void OnTabSelected(string tabName)
+    {
+        _currentTab = tabName;
+        UpdateTabButtonVisuals();
+    }
+
+    private void UpdateTabButtonVisuals()
+    {
+        foreach(var tabButton in _tabButtons)
+        {
+            if(tabButton.TabName == _currentTab)
+            {
+                tabButton.SetGraphicOpacity(1f);
+            }
+            else
+            {
+                tabButton.SetGraphicOpacity(_unselectedTabOpacity);
+                CustomDebug.Log($"{tabButton.name}, {_unselectedTabOpacity}");
+            }
+        }
+    }
+
+
+}
---
+++ Assets/02. Scripts/UI/Options/UI_OptionView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class UI_OptionView : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private List<UI_OptionTabButtonView> _tabButtons;
+
+    [Header("Settings")]
+    [SerializeField] private float _unselectedTabOpacity = 0.8f;
+
+    private UI_OptionPresenter _presenter;
+
+    private void Awake()
+    {
+        InitializePresenter();
+    }
+
+    private void Start()
+    {
+        _presenter.Start();
+    }
+
+    private void InitializePresenter()
+    {
+        Assert.IsNotNull(_tabButtons, "Tab buttons list reference is null!");
+        Assert.IsTrue(_tabButtons.Count > 0, "Tab buttons list is empty in the inspector!");
+        _presenter = new UI_OptionPresenter(this, _tabButtons, _unselectedTabOpacity);
+    }
+}
