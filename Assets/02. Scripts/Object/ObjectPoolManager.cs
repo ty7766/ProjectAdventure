e2185d723f9711316fc49ccddde069dba3bd2191
hyojoon Lim <hyojoon42@naver.com>
2026-05-11 21:31:10 +0900

    revert : conflict 복원

--- Assets/02. Scripts/Object/ObjectPoolManager.cs
+++ Assets/02. Scripts/Object/ObjectPoolManager.cs
@@ -92,7 +92,7 @@ public class ObjectPoolManager : Singleton<ObjectPoolManager>
         }
     }
 
-    private bool CreateNewObject(PoolObjectType type, GameObject prefab = null)
+    private void CreateNewObject(PoolObjectType type, GameObject prefab = null)
     {
         if (prefab == null)
         {
@@ -104,12 +104,10 @@ public class ObjectPoolManager : Singleton<ObjectPoolManager>
             GameObject obj = Instantiate(prefab, transform);
             obj.SetActive(false);
             _poolDictionary[type].Enqueue(obj);
-            return true;
         }
         else
         {
             CustomDebug.LogWarning($"[ObjectPoolManager] {type} 생성 실패(프리팹 누락 가능).");
-            return false;
         }
     }
 
@@ -126,8 +124,10 @@ public class ObjectPoolManager : Singleton<ObjectPoolManager>
         }
 
         //풀이 비어 있으면 1개 새로 생성
-        if(!CreateNewObject(type))
+        CreateNewObject(type);
+        if (pool.Count == 0)
         {
+            CustomDebug.LogWarning($"[ObjectPoolManager] {type} 생성 실패(프리팹 누락 가능)");
             return null;
         }
         return pool.Dequeue();
